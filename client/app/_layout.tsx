560aef3be4213f6b7853e746b0a9e1560a89957b
Archita Panda <pandaarchita6@gmail.com>
2026-04-30 16:08:25 +0530

    Product info page

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -4,6 +4,7 @@ import '@/global.css'
 import { CartProvider } from "@/context/CartContext";
 import { WishlistProvider } from "@/context/WishlistContext";
 import {GestureHandlerRootView} from 'react-native-gesture-handler'
+import Toast from "react-native-toast-message";
 
 if (Platform.OS === "web") {
   (
@@ -18,7 +19,8 @@ export default function RootLayout() {
       <GestureHandlerRootView style={{flex: 1}}>
         <CartProvider>
         <WishlistProvider>
-          <Stack screenOptions={{headerShown: false}}/>;
+          <Stack screenOptions={{headerShown: false}}/>
+          <Toast />
         </WishlistProvider>
       </CartProvider>
       </GestureHandlerRootView>
